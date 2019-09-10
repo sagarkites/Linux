c681a952d56299b68665e03f17bca3cb7af101e8
vidyasagar <33413645+sagarkites@users.noreply.github.com>
2019-09-10 15:19:36 +0530

    Update os.py

--- python/os.py
+++ python/os.py
@@ -1,2 +1,9 @@
 import os
-os.system('sudo cat /etc/passwd')
+os.system('sudo cat /etc/passwd') # Runs command 
+os.getcwd() # Returns the current working directory.
+os.listdir(path) # Return a list of the entries in the directory given by path.
+os.mkdir(path) # Create a directory named path with numeric mode mode.
+os.remove(path) # Remove (delete) the file path.
+os.umask(mask) # Set the current numeric umask and return the previous umask.
+os.getpid() # Returns the real process ID of the current process.
+os.environ() # Get the users environment 
