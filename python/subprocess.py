01078ef797724339b272e3c71bc57793e7b60f23
vidyasagar <33413645+sagarkites@users.noreply.github.com>
2019-09-11 13:00:01 +0530

    Update subprocess.py
    sub

--- python/subprocess.py
+++ python/subprocess.py
@@ -1,7 +1,20 @@
 import subprocess
-i=subprocess.call("ls -altr", shell=True) # Gives output with exit status shell=True says /bin/sh -c
-j=subprocess.check_call(["mkdir", "sagar"]) #Gives the output without exit status
-print i
-if j == 0: 
-   subprocess.call('touch /home/vagrant/sagar/scott.txt', shell=True)
-   
+
+i = subprocess.run("ls", shell=True) # executes with returncode
+
+j = subprocess.run(["ls", "-altr"],capture_output=True)
+
+print(j.stdout.decode()) # prints stdout
+
+print(i)
+
+print(j.stderr.decode()) # prints stderr
+
+k = subprocess.run(["cat","sub.py"], capture_output=True,)
+
+l = subprocess.run(["grep","-n", "k"],capture_output=True, input=k.stdout) # using stdout as stdinput
+
+print(l.stdout.decode())
+
+if j.returncode != 0:
+   print("oops! check the code")
